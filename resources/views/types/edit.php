927d452201c5539557f37787e4f20691d6068b48
Adam Thomas <thomasadam83@hotmail.com>
2022-04-03 22:19:27 -0400

    Added types edit

--- resources/views/types/edit.php
+++ resources/views/types/edit.php
@@ -34,57 +34,27 @@
 
         <section class="w3-padding">
 
-            <h2>Edit User</h2>
+            <h2>Edit Type</h2>
 
-            <form method="post" action="/console/users/edit/<?= $user->id ?>" novalidate class="w3-margin-bottom">
+            <form method="post" action="/console/types/edit/<?= $type->id ?>" novalidate class="w3-margin-bottom">
 
                 <?= csrf_field() ?>
 
                 <div class="w3-margin-bottom">
-                    <label for="first">First Name:</label>
-                    <input type="text" name="first" id="first" value="<?= old('first', $user->first) ?>" required>
+                    <label for="title">Title:</label>
+                    <input type="text" name="title" id="title" value="<?= old('title', $type->title) ?>" required>
                     
-                    <?php if($errors->first('first')): ?>
+                    <?php if($errors->first('title')): ?>
                         <br>
-                        <span class="w3-text-red"><?= $errors->first('first'); ?></span>
+                        <span class="w3-text-red"><?= $errors->first('title'); ?></span>
                     <?php endif; ?>
                 </div>
 
-                <div class="w3-margin-bottom">
-                    <label for="last">Last Name:</label>
-                    <input type="text" name="last" id="last" value="<?= old('last', $user->last) ?>" required>
-                    
-                    <?php if($errors->first('last')): ?>
-                        <br>
-                        <span class="w3-text-red"><?= $errors->first('last'); ?></span>
-                    <?php endif; ?>
-                </div>
-
-                <div class="w3-margin-bottom">
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" id="email" value="<?= old('email', $user->email) ?>">
-
-                    <?php if($errors->first('email')): ?>
-                        <br>
-                        <span class="w3-text-red"><?= $errors->first('email'); ?></span>
-                    <?php endif; ?>
-                </div>
-
-                <div class="w3-margin-bottom">
-                    <label for="password">Password:</label>
-                    <input type="password" name="password" id="password">
-
-                    <?php if($errors->first('password')): ?>
-                        <br>
-                        <span class="w3-text-red"><?= $errors->first('password'); ?></span>
-                    <?php endif; ?>
-                </div>
-
-                <button type="submit" class="w3-button w3-green">Edit User</button>
+                <button type="submit" class="w3-button w3-green">Edit Type</button>
 
             </form>
 
-            <a href="/console/users/list">Back to User List</a>
+            <a href="/console/types/list">Back to Type List</a>
 
         </section>
 
