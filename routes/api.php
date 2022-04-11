c93c355c07b671c5b03e6cb87744cbf84e7cd198
Adam Thomas <thomasadam83@hotmail.com>
2022-04-11 10:18:58 -0400

    Added API to CMS

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Type;
+use App\Models\User;
+use App\Models\Project;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,44 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/types', function(){
+
+    $types = Type::orderBy('title')->get();
+    return $types;
+
+});
+
+Route::get('/projects', function(){
+
+    $projects = Project::orderBy('created_at')->get();
+
+    foreach($projects as $key => $project)
+    {
+        $projects[$key]['user'] = User::where('id', $project['user_id'])->first();
+        $projects[$key]['type'] = Type::where('id', $project['type_id'])->first();
+
+        if($project['image'])
+        {
+            $projects[$key]['image'] = env('APP_URL').'storage/'.$project['image'];
+        }
+    }
+
+    return $projects;
+
+});
+
+Route::get('/projects/profile/{project?}', function(Project $project){
+
+    $project['user'] = User::where('id', $project['user_id'])->first();
+    $project['type'] = Type::where('id', $project['type_id'])->first();
+
+    if($project['image'])
+    {
+        $project['image'] = env('APP_URL').'storage/'.$project['image'];
+    }
+
+    return $project;
+
+});
+
