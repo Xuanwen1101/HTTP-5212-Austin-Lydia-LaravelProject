a94decc4c1b4b44e0e0e2127877f18997169486e
AC59852 <austin.caron1@gmail.com>
2022-08-01 20:27:37 -0400

    ported styles for image, header and login page

--- resources/views/console/login.php
+++ resources/views/console/login.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>My Portfolio</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600;700&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="/app.css">
@@ -15,25 +18,26 @@
     <body>
 
 
-        <header class="w3-padding">
-
-            <h1 class="w3-text-red">Portfolio Console</h1>
-
-            <a href="/">Return to My Portfolio</a>
-
+        <header class="header">
+            <img class="nav__btn" src="/" alt="">
+            <nav class="header__nav">
+              <ul class="nav__wrapper">
+                <li class="nav__item"><a href="/">Website Home Page</a></li>
+              </ul>
+            </nav>
         </header>
 
         <hr>
 
-        <section class="w3-padding">
+        <section class="objects-container">
 
             <form method="post" action="/console/login" novalidate>
 
                 <?= csrf_field() ?>
 
-                <div class="w3-margin-bottom">
-                    <label for="email">Email Address:</label>
-                    <input type="email" name="email" id="email" value="<?= old('email') ?>" required>
+                <div class="form__field">
+                    <label for="email" class="form__label">Email Address:</label>
+                    <input type="email" class="form__input" name="email" id="email" value="<?= old('email') ?>" required>
                     
                     <?php if($errors->first('email')): ?>
                         <br>
@@ -41,17 +45,21 @@
                     <?php endif; ?>
                 </div>
 
-                <div class="w3-margin-bottom">
-                    <label for="password">Password:</label>
-                    <input type="password" name="password" id="password" required>
+                <br>
+
+                <div class="form__field">
+                    <label for="password" class="form__label">Password:</label>
+                    <input type="password" class="form__input" name="password" id="password" required>
 
                     <?php if($errors->first('password')): ?>
                         <br>
                         <span class="w3-text-red"><?= $errors->first('password'); ?></span>
                     <?php endif; ?>
                 </div>
+                
+                <br>
 
-                <button type="submit">Log In</button>
+                <button type="submit" class="form__button">Log In</button>
 
             </form>
 
--- resources/views/projects/image.php
+++ resources/views/projects/image.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>My Portfolio</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600;700&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="/app.css">
@@ -13,54 +16,49 @@
         
     </head>
     <body>
-
-
-        <header class="w3-padding">
-
-            <h1 class="w3-text-red">Portfolio Console</h1>
-
-            <?php if(Auth::check()): ?>
-                You are logged in as <?= auth()->user()->first ?> <?= auth()->user()->last ?> | 
-                <a href="/console/logout">Log Out</a> | 
-                <a href="/console/dashboard">Dashboard</a> | 
-                <a href="/">Website Home Page</a>
-            <?php else: ?>
-                <a href="/">Return to My Portfolio</a>
-            <?php endif; ?>
-
+        <header class="header">
+            <img class="nav__btn" src="/" alt="">
+            <nav class="header__nav">
+              <ul class="nav__wrapper">
+                <li class="nav__close">X</li>
+                <li class="nav__item"><a href="/console/dashboard">Dashboard</a></li>
+                <li class="nav__line">|</li>
+                <li class="nav__item"><a href="/">Website Home Page</a></li>
+                <li class="nav__line">|</li>
+                <li class="nav__item"><a href="/console/logout">Log Out</a></li>
+              </ul>
+            </nav>
         </header>
 
-        <hr>
+        <section>
 
-        <section class="w3-padding">
+            <h2 class="title">Project Image</h2>
 
-            <h2>Project Image</h2>
-
-            <div class="w3-margin-bottom">
+            <div class="objects-container">
                 <?php if($project->image): ?>
                     <img src="<?= asset('storage/'.$project->image) ?>" width="200">
                 <?php endif; ?>
             </div>
 
-            <form method="post" action="/console/projects/image/<?= $project->id ?>" novalidate class="w3-margin-bottom" enctype="multipart/form-data">
+            <div class="objects-container">
+              <form method="post" action="/console/projects/image/<?= $project->id ?>" novalidate class="form" enctype="multipart/form-data">
+                  <?= csrf_field() ?>
+                  <div class="form__field">
+                      <label for="image" class="form__label">Image:</label>
+                      <input type="file" class="form__input" name="image" id="image" value="<?= old('image') ?>" required>
+              
+                      <?php if($errors->first('image')): ?>
+                          <br>
+                          <span class="w3-text-red"><?= $errors->first('image'); ?></span>
+                      <?php endif; ?>
+                  </div>
+                  <button type="submit" class="form__button">Add Image</button>
+              </form>
+            </div>
 
-                <?= csrf_field() ?>
-
-                <div class="w3-margin-bottom">
-                    <label for="image">Image:</label>
-                    <input type="file" name="image" id="image" value="<?= old('image') ?>" required>
-                    
-                    <?php if($errors->first('image')): ?>
-                        <br>
-                        <span class="w3-text-red"><?= $errors->first('image'); ?></span>
-                    <?php endif; ?>
-                </div>
-
-                <button type="submit" class="w3-button w3-green">Add Image</button>
-
-            </form>
-
-            <a href="/console/projects/list">Back to Project List</a>
+            <div class="object__link">
+              <a href="/console/projects/list">Back to Project List</a>
+            </div>
 
         </section>
 
