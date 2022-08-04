af78920017230e6a6c1b5167da84fae87d3e9f5b
AC59852 <austin.caron1@gmail.com>
2022-08-04 13:02:50 -0400

    added api routes for each page, along with dynamic routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Type;
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Email;
+use App\Models\Blog;
+use App\Models\Content;
+use App\Models\School;
+use App\Models\Skill;
+use App\Models\Work;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,3 +68,167 @@ Route::get('/projects/profile/{project?}', function(Project $project){
 
 });
 
+Route::get('/emails', function(){
+
+  $emails = Email::orderBy('created_at')->get();
+  return $emails;
+
+});
+
+// dynamic route for email content
+Route::get('/emails/{email}', function(Email $email){
+
+  $email['content'] = Content::where('id', $email['id'])->first();
+  return $email;
+
+});
+
+Route::get('/blogs', function(){
+
+  $blogs = Blog::orderBy('created_at')->get();
+  
+  foreach ($blogs as $key => $blog) {
+    $blogs[$key]['user'] = User::where('id', $blog['user_id'])->first();
+    
+    if($blog['image'])
+    {
+        $blogs[$key]['image'] = env('APP_URL').'storage/'.$blog['image'];
+    }
+  }
+
+  return $blogs;
+
+});
+
+// dynamic route for blog content
+Route::get('/blogs/profile/{blog?}', function(Blog $blog){
+
+  $blog['content'] = Content::where('id', $blog['id'])->first();
+  $blog['user'] = User::where('id', $blog['user_id'])->first();
+
+  if($blog['image'])
+  {
+      $blog['image'] = env('APP_URL').'storage/'.$blog['image'];
+  }
+
+  return $blog;
+
+});
+
+Route::get('/schools', function(){
+
+  $schools = School::orderBy('created_at')->get();
+
+  foreach ($schools as $key => $school) {
+    
+    if($school['image'])
+    {
+        $schools[$key]['image'] = env('APP_URL').'storage/'.$school['image'];
+    }
+  }
+
+  return $schools;
+
+});
+
+// dynamic route for school content
+Route::get('/schools/{school}', function(School $school){
+
+  $school['content'] = Content::where('id', $school['id'])->first();
+
+  if($school['image'])
+  {
+      $school['image'] = env('APP_URL').'storage/'.$school['image'];
+  }
+
+  return $school;
+
+});
+
+Route::get('/skills', function(){
+
+  $skills = Skill::orderBy('created_at')->get();
+
+  foreach ($skills as $key => $skill) {
+    
+    if($skill['image'])
+    {
+        $skills[$key]['image'] = env('APP_URL').'storage/'.$skill['image'];
+    }
+  }
+
+  return $skills;
+
+});
+
+// dynamic route for skill content
+Route::get('/skills/{skill}', function(Skill $skill){
+
+  $skill['content'] = Content::where('id', $skill['id'])->first();
+
+  if($skill['image'])
+  {
+      $skill['image'] = env('APP_URL').'storage/'.$skill['image'];
+  }
+
+  return $skill;
+
+});
+
+Route::get('/works', function(){
+
+  $works = Work::orderBy('created_at')->get();
+
+  foreach ($works as $key => $work) {
+    
+    if($work['image'])
+    {
+        $works[$key]['image'] = env('APP_URL').'storage/'.$work['image'];
+    }
+  }
+
+  return $works;
+
+});
+
+// dynamic route for work content
+Route::get('/works/{work}', function(Work $work){
+
+  $work['content'] = Content::where('id', $work['id'])->first();
+
+  if($work['image'])
+  {
+      $work['image'] = env('APP_URL').'storage/'.$work['image'];
+  }
+
+  return $work;
+
+});
+
+Route::get('/extras', function(){
+
+  $extras = Content::orderBy('created_at')->get();
+
+  foreach ($extras as $key => $extra) {
+    
+    if($extra['image'])
+    {
+        $extras[$key]['image'] = env('APP_URL').'storage/'.$extra['image'];
+    }
+  }
+
+  return $extras;
+
+});
+
+// dynamic route for extra content
+Route::get('/extras/{extra}', function(Content $extra){
+
+  if($extra['image'])
+  {
+      $extra['image'] = env('APP_URL').'storage/'.$extra['image'];
+  }
+
+  return $extra;
+
+});
