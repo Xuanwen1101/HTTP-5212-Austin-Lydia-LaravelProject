57db242dc65d9a6bfad9901dfcf5aa2e58834430
LAPTOP-56J4U8VG\lydia <xuanwen1101@outlook.com>
2022-08-03 21:07:38 -0400

    Update mass assignment for data models.

--- app/Models/School.php
+++ app/Models/School.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'school',
+        'school_url',
+        'major',
+        'degree',
+        'start_date',
+        'end_date',
+        'image',
+    ];
+    
 }
--- app/Models/Work.php
+++ app/Models/Work.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Work extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'employment_type',
+        'company_name',
+        'location',
+        'description',
+        'start_date',
+        'end_date',
+        'image',
+    ];
+    
 }
